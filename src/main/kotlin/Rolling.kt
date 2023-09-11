b9e104a049a2a0e3a326c829512aaa249958970d
Malox <Maxi.Junges@gmx.de>
2023-09-11 04:28:03 +0200

    add batch rolling

--- src/main/kotlin/Rolling.kt
+++ src/main/kotlin/Rolling.kt
@@ -9,15 +9,19 @@ class Rolling() {
     private val uiHandler = UIHandler()
     private val summonButtonLocation = uiHandler.findSummonButton() ?: error("Couldn't find summon button")
 
-    fun rolling() {
-        for (i in 1..1000) {
-            val rarities = doOneRoll()
-            if (rarities.sixCount >= 3
-                || (rarities.sixCount + rarities.fiveCount) >= 6
-                || (rarities.sixCount >= 2 && rarities.fiveCount >= 3)) {
-                screenshotRoll("${rarities.sixCount}-${rarities.fiveCount}-${System.currentTimeMillis()}")
-            }
+    fun rolling(batchCount: Int = 10000, batchSize: Int = 30) {
+        for (i in 1..batchCount) {
+            rollOneBatch(batchSize)
         }
+    }
+
+    private fun rollOneBatch(batchSize: Int) {
+        for (i in 1..batchSize) {
+            val rarities = doOneRoll()
+            screenshotRareRoll(rarities)
+        }
+        Thread.sleep(500)
+        resetRolls()
     }
 
     private fun doOneRoll(): Rarities {
@@ -38,6 +42,14 @@ class Rolling() {
         val lowCount: Int,
     )
 
+    private fun screenshotRareRoll(rarities: Rarities) {
+        if (rarities.sixCount >= 3
+            || (rarities.sixCount + rarities.fiveCount) >= 6
+            || (rarities.sixCount >= 2 && rarities.fiveCount >= 3)) {
+            screenshotRoll("${rarities.sixCount}-${rarities.fiveCount}-${System.currentTimeMillis()}")
+        }
+    }
+
     private fun screenshotRoll(screenshotName: String) {
         val startingPoint = summonButtonLocation + (-5 to -5)
 
@@ -48,8 +60,13 @@ class Rolling() {
         ImageIO.write(screenshot, "jpg", file)
     }
 
+    fun resetRolls() {
+        robot.click(summonButtonLocation + resetButtonOffset, 100)
+    }
+
     companion object {
         private const val screenshotFolder = "screenshots/"
+        val resetButtonOffset = 20 to 750
     }
 }
 
@@ -60,9 +77,3 @@ fun Robot.click(point: Pair<Int, Int>, delay: Long = 0) {
     Thread.sleep(delay)
     this.mouseRelease(MouseEvent.BUTTON1_DOWN_MASK)
 }
-
-
-
-
-
-
